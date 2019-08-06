9d4a796b5ca1e3b238fc6b8f6a9d2435823a7425
Amruth Nare <amruthnare.1@gmail.com>
2019-08-06 18:04:53 -0400

    gg commit

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ const fs = require('fs');
 const path = require('path');
 const Time = require('date-and-time');
 const time = require('time');
-var filePath = path.join(__dirname, 'secret.txt');
+var filePath = path.join(__dirname, 'secret.txt'); //sike u aint getting that
 var token = fs.readFileSync(filePath, "utf8");
 var now = new time.Date();
 const PREFIX = '?';
-//const guild = bot.guilds.get("491014592688947220");
-
-
-//console.log(token);
 
 /*
 fs.readFile(filePath, 'utf8', function(err, contents) {
@@ -30,11 +26,7 @@ bot.on('ready', function() {
 });
 
 bot.on('message', function(msg) {
-    /*
-    if (msg.content == "Testing") {
-        msg.reply("Looks Good!");
-    }
-    */
+
     now.setTimezone("America/New_York");
     let args = msg.content.substring(PREFIX.length).split(" "); //returns the text after the prefix
     //console.log(args);
@@ -80,7 +72,7 @@ bot.on('message', function(msg) {
         var person = mems[Math.floor(Math.random() * mems.length)];
         msg.channel.sendMessage(person);
     }
-    if (arg == "is") {
+    if (arg == "is" || "will") {
         var answer;
         if (Math.floor(Math.random() * 10) >= 5) {
             answer = "yes";
@@ -89,39 +81,6 @@ bot.on('message', function(msg) {
         }
         msg.channel.sendMessage(answer);
     }
-
-
-    //code is trash
-    /*
-    switch (args[0]) {
-        case 'date':
-            var hours = now.getHours() % 12;
-            var spec;
-            if (now.getHours >= 12) {
-                spec = "pm";
-            } else {
-                spec = "am";
-            }
-            var date = now.getDate();
-
-            msg.channel.sendMessage("The date is the " + now.getDate() + "th \nThe hour is " + hours + spec);
-            //console.log(args[0]);
-            break;
-        case 'test':
-            msg.channel.sendMessage("Nice bruh");
-            break;
-        case 'help':
-            msg.channel.sendMessage("Availible commands are scuffed(?date, ?test, ?git). This was a waste of my time. If you do ?(Insert Who Question) it will return a person from server. So scuffed smh.");
-            break;
-        case 'git':
-            msg.channel.sendMessage("https://github.com/amruth21")
-            break;
-        default:
-            var person = "Datta"
-            msg.channel.sendMessage(person)
-            break;
-    }
-    */
 });
 
 bot.login(token);
